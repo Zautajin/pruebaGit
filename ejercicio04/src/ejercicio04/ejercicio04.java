a851d5737d34b99557568afbef0d9a0b0cd6479e
Zautajin <58167053+Zautajin@users.noreply.github.com>
2020-03-17 11:54:06 +0100

    Preparado para bajada

--- ejercicio04/src/ejercicio04/ejercicio04.java
+++ ejercicio04/src/ejercicio04/ejercicio04.java
@@ -5,6 +5,8 @@ public class ejercicio04 {
 	public static void main(String[] args) {
 //		Primera subida a GitHub
 		System.out.println("Hola mundo");
+
+//		Bajamos esta nueva versión desde GitHub		
 		
 	}
 
